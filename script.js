4f6ee55292e8d4effc4c28afbddef48a1b680039
Jerome Foster II <jfoster@wlapcs.org>
2018-02-01 01:05:03 +0000

    updated the world

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
-function succeed(){
-    document.getElementById("home").setAttribute("visible","false");
-    document.getElementById("panda").setAttribute("visible","true");
-    document.getElementById("fracking").setAttribute("visible","false");
-        document.getElementById("sceneAfrica-water").setAttribute("visible","false");
-}
-
-function rac(){
-    document.getElementById("home").setAttribute("visible","false");
-    document.getElementById("panda").setAttribute("visible","false");
-    document.getElementById("fracking").setAttribute("visible","true");
-        document.getElementById("sceneAfrica-water").setAttribute("visible","false");
-}
-
-
-function (){
-    document.getElementById("panda").setAttribute("visible","false");
+function home(){
     document.getElementById("home").setAttribute("visible","true");
-    document.getElementById("fracking").setAttribute("visible","false");
-        document.getElementById("sceneAfrica-water").setAttribute("visible","false");
+    document.getElementById("the-border-wall").setAttribute("visible","false");
+    document.getElementById("gsScene").setAttribute("visible","false");
+    document.getElementById("ActivScene").setAttribute("visible","false");
+    document.getElementById("storiesScene").setAttribute("visible","false");
 }
 
-function travelToAfricawater(){
+function borderWallScene(){
     document.getElementById("home").setAttribute("visible","false");
-    document.getElementById("sceneAfrica-water").setAttribute("visible","true");
-    document.getElementById("fracking").setAttribute("visible","false");
-    document.getElementById("panda").setAttribute("visible","false");
+    document.getElementById("the-border-wall").setAttribute("visible","true");
+    document.getElementById("gsScene").setAttribute("visible","false");
+    document.getElementById("ActivScene").setAttribute("visible","false");
+    document.getElementById("storiesScene").setAttribute("visible","false");
+}
+
+function gsLink(){
+    document.getElementById("home").setAttribute("visible","false");
+    document.getElementById("the-border-wall").setAttribute("visible","false");
+    document.getElementById("gsScene").setAttribute("visible","true");
+    document.getElementById("ActivScene").setAttribute("visible","false");
+    document.getElementById("storiesScene").setAttribute("visible","false");
+}
+
+function storiesButton(){
+    document.getElementById("home").setAttribute("visible","false");
+    document.getElementById("the-border-wall").setAttribute("visible","false");
+    document.getElementById("gsScene").setAttribute("visible","false");
+    document.getElementById("ActivScene").setAttribute("visible","false");
+    document.getElementById("storiesScene").setAttribute("visible","true");
+}
+
+function ActivButton(){
+    document.getElementById("home").setAttribute("visible","false");
+    document.getElementById("the-border-wall").setAttribute("visible","false");
+    document.getElementById("gsScene").setAttribute("visible","false");
+    document.getElementById("ActivScene").setAttribute("visible","true");
+    document.getElementById("storiesScene").setAttribute("visible","false");
+}
+
+function mutevideo(){
+    document.getElementById("home-sky-video").setAttribute("muted", "true");
+}
+
+function ljobs(){
+    document.getElementById("home").setAttribute("visible","false");
+    document.getElementById("the-border-wall").setAttribute("visible","false");
+    document.getElementById("gsScene").setAttribute("visible","false");
+    document.getElementById("ActivScene").setAttribute("visible","true");
+    document.getElementById("storiesScene").setAttribute("visible","false");
+    document.getElementById("ljobsBio").setAttribute("visible","true");
+    document.getElementById("ljobs-plus").setAttribute("visible","false");
+}
+
+function jmurguia(){
+    document.getElementById("home").setAttribute("visible","false");
+    document.getElementById("the-border-wall").setAttribute("visible","false");
+    document.getElementById("gsScene").setAttribute("visible","false");
+    document.getElementById("ActivScene").setAttribute("visible","true");
+    document.getElementById("storiesScene").setAttribute("visible","false");
+    document.getElementById("jmurguiaBio").setAttribute("visible","true");
+    document.getElementById("jmurguia-plus").setAttribute("visible","false");
+
 }
